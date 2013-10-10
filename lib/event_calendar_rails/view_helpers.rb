e0ece7d02963e275481d41a3a72c4be395ca6310
Alex Takitani <aftakitani@gmail.com>
2013-10-10 17:02:20 -0300

    full week

--- lib/event_calendar_rails/view_helpers.rb
+++ lib/event_calendar_rails/view_helpers.rb
@@ -7,13 +7,19 @@ module EventCalendarRails
         Calendar.new(self, date, events, "month", nil, nil, nil, nil , display_mode=:regular, block).table
       end
 
-      def week_calendar(date = Date.today, events=nil, start_time = Time.parse("08:00"), end_time = Time.parse("19:30"), interval=30.minutes, precedence=1.hour, display_mode=:regular, &block)
+      def week_calendar(date = Date.today, events=nil, start_time = Time.parse("08:00"), end_time = Time.parse("19:30"), interval=30.minutes, precedence=1.hour,
+        display_mode=:regular, &block)
         Calendar.new(self, date, events, "week", start_time, end_time, interval, precedence, display_mode, block ).table
       end
 
       def week_calendar_admin(date = Date.today, events=nil, start_time = Time.parse("08:00"), end_time = Time.parse("19:30"), interval=30.minutes, precedence=1.hour, display_mode=:admin, &block)
         Calendar.new(self, date, events, "week", start_time, end_time, interval, precedence, display_mode, block ).table
       end
+
+      def week_calendar_full_day(date = Date.today, events=nil , &block)
+        Calendar.new(self, date, events, "week_full", nil, nil, nil, nil, :regular, block ).table
+      end
+
 
       class Calendar < Struct.new(:view, :date, :events, :mode, :start_time, :end_time, :interval, :precedence, :display_mode, :callback)
         HEADER = %w[Domingo Segunda Terça Quarta Quinta Sexta Sábado]
@@ -41,24 +47,43 @@ module EventCalendarRails
 
         def time_rows
           return if mode == "month"
-          (start_time.to_i..end_time.to_i).step(interval).map do |h|
-            content_tag(:tr, :class=>h) do
-              8.times.map do |i|
-                content_tag :td do
-                  if i == 0
-                    I18n.localize(Time.at(h),:format=>"%H:%M")
-                  else
-                    action_div(weeks[0][i-1],h,h+interval)
+          if mode=="week_full"
+            content_tag(:tr, :class=>'full_row') do
+                7.times.map do |i|
+                  content_tag :td do
+                      full_action_div(weeks[0][i-1])
                   end
-                end
-              end.join.html_safe
-            end
-          end.join.html_safe
+                end.join.html_safe
+              end
+          else
+            (start_time.to_i..end_time.to_i).step(interval).map do |h|
+              content_tag(:tr, :class=>h) do
+                8.times.map do |i|
+                  content_tag :td do
+                    if i == 0
+                      I18n.localize(Time.at(h),:format=>"%H:%M")
+                    else
+                      action_div(weeks[0][i-1],h,h+interval)
+                    end
+                  end
+                end.join.html_safe
+              end
+            end.join.html_safe
+          end
+        end
+
+        def full_action_div(date)
+
+          unless events.nil?
+            events.find_all{|e| e[0]==date.to_time.beginning_of_day.to_i}.map do |ev|
+                content_tag(:div, "", :class =>"event",
+                            :data=> ev.last, :style=>"width:95%;" )
+            end.join.html_safe
+          end
         end
 
         def action_div(date,start,endt)
           cur_date = DateTime.parse(date.to_s + " 00:00:00 -0300")
-          #binding.pry if cur_date == DateTime.parse(Date.today.to_s + " 00:00:00")
           start_time = DateTime.parse(date.to_s + " " + Time.at(start).strftime("%H:%M") + "-0300")
           end_time = DateTime.parse(date.to_s + " " + Time.at(endt).strftime("%H:%M") + "-0300")
           now = DateTime.parse DateTime.now.strftime("%Y-%m-%d %H:%M -0300")
@@ -67,7 +92,6 @@ module EventCalendarRails
                start_time.to_i,
                end_time.to_i]
 
-          #binding.pry
           unless events.nil?
             if ev=events.find{|i| i[0..2]==ar}
 
@@ -87,7 +111,6 @@ module EventCalendarRails
         end
 
         def footer
-
           weeks.map do |week|
             content_tag :tr do
               content_tag(:td,'Totais',:class=>'direita') +
@@ -102,9 +125,8 @@ module EventCalendarRails
         end
 
         def header
-          #binding.pry
+          HEADER.unshift("") unless mode == "week_full"
           content_tag :tr do
-            content_tag(:th,'',:style=>"width:12.5%") +
             HEADER.map { |day| content_tag :th, day,:style=>"width:12.5%" }.join.html_safe
           end
         end
@@ -112,7 +134,6 @@ module EventCalendarRails
         def week_rows
           weeks.map do |week|
             content_tag :tr do
-              content_tag(:td,'Dia',:class=>'direita') +
               week.map { |day| day_cell(day) }.join.html_safe
             end
           end.join.html_safe
@@ -134,7 +155,7 @@ module EventCalendarRails
           if mode == "month"
             first = date.to_date.beginning_of_month.beginning_of_week(START_DAY)
             last = date.to_date.end_of_month.end_of_week(START_DAY)
-          elsif mode == "week"
+          elsif mode == "week" || mode == "week_full"
             first = date.to_date.beginning_of_week(START_DAY)
             last = date.to_date.end_of_week(START_DAY)
           end
